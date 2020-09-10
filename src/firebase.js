cf8b540f4b4f78be6a0d0565d15b981b873e38bb
simonjv01 <sjvargas01@gmail.com>
2020-09-09 23:16:55 -0400

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBTo96enX30qnpLJdoT2-ko-WCDDfa9Uq0",
+    authDomain: "clone-4bf86.firebaseapp.com",
+    databaseURL: "https://clone-4bf86.firebaseio.com",
+    projectId: "clone-4bf86",
+    storageBucket: "clone-4bf86.appspot.com",
+    messagingSenderId: "55704563940",
+    appId: "1:55704563940:web:8cd4e24249e74c103d3cf1",
+    measurementId: "G-S7R65Y54RN"
+  };
